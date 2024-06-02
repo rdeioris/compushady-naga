549b70e1e180664fa87c1ab2d138dd9619076bfc
rdeioris <roberto.deioris@gmail.com>
2024-06-02 15:17:59 +0200

    added registers remapping for hlsl

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use std::ptr::null;
+use std::{collections::HashMap, ptr::null};
+
+use naga::GlobalVariable;
 
 fn compushady_naga_get_utf8(ptr: *const u8, len: usize) -> String {
     return unsafe { std::str::from_utf8_unchecked(std::slice::from_raw_parts(ptr, len)) }
@@ -41,8 +43,153 @@ fn compushady_naga_module_to_hlsl(
             }
         }
         Ok(info) => {
+            let mut options = naga::back::hlsl::Options::default();
+
+            let mut register_b: u32 = 0;
+            let mut register_t: u32 = 0;
+            let mut register_u: u32 = 0;
+            let mut register_s: u32 = 0;
+
+            let mut ordered_globals = HashMap::<u32, Vec<&GlobalVariable>>::new();
+            // retrieve resources
+            for global_variable in module.global_variables.iter() {
+                match &global_variable.1.binding {
+                    None => {}
+                    Some(binding) => {
+                        if !ordered_globals.contains_key(&binding.group) {
+                            ordered_globals.insert(binding.group, Vec::new());
+                        }
+                        ordered_globals
+                            .get_mut(&binding.group)
+                            .unwrap()
+                            .push(global_variable.1);
+                    }
+                }
+            }
+
+            let mut group_keys: Vec<&u32> = ordered_globals.keys().collect();
+            group_keys.sort();
+
+            for group_key in group_keys {
+                let mut space_ordered_globals = HashMap::<u32, &GlobalVariable>::new();
+                for &global_variable in ordered_globals.get(group_key).unwrap() {
+                    let key = global_variable.binding.as_ref().unwrap();
+                    space_ordered_globals.insert(key.binding, global_variable);
+                }
+
+                let mut keys: Vec<&u32> = space_ordered_globals.keys().collect();
+                keys.sort();
+
+                for key in keys {
+                    let global_variable = space_ordered_globals[key];
+
+                    let inner = &module.types[global_variable.ty].inner;
+
+                    match &global_variable.space {
+                        naga::AddressSpace::Uniform => {
+                            let binding = global_variable.binding.clone().unwrap();
+                            options.binding_map.insert(
+                                binding,
+                                naga::back::hlsl::BindTarget {
+                                    space: 0,
+                                    register: register_b,
+                                    binding_array_size: None,
+                                },
+                            );
+                            register_b += 1
+                        }
+                        naga::AddressSpace::Storage { access } => {
+                            if access.contains(naga::StorageAccess::STORE) {
+                                let binding = global_variable.binding.clone().unwrap();
+                                options.binding_map.insert(
+                                    binding,
+                                    naga::back::hlsl::BindTarget {
+                                        space: 0,
+                                        register: register_u,
+                                        binding_array_size: None,
+                                    },
+                                );
+                                register_u += 1
+                            } else {
+                                let binding = global_variable.binding.clone().unwrap();
+                                options.binding_map.insert(
+                                    binding,
+                                    naga::back::hlsl::BindTarget {
+                                        space: 0,
+                                        register: register_t,
+                                        binding_array_size: None,
+                                    },
+                                );
+                                register_t += 1
+                            }
+                        }
+                        naga::AddressSpace::Handle => {
+                            let handle_ty = match *inner {
+                                naga::TypeInner::BindingArray { ref base, .. } => {
+                                    &module.types[*base].inner
+                                }
+                                _ => inner,
+                            };
+                            match *handle_ty {
+                                naga::TypeInner::Sampler { .. } => {
+                                    let binding = global_variable.binding.clone().unwrap();
+                                    options.binding_map.insert(
+                                        binding,
+                                        naga::back::hlsl::BindTarget {
+                                            space: 0,
+                                            register: register_s,
+                                            binding_array_size: None,
+                                        },
+                                    );
+                                    register_s += 1
+                                }
+                                naga::TypeInner::Image {
+                                    class: naga::ImageClass::Storage { .. },
+                                    ..
+                                } => {
+                                    let binding = global_variable.binding.clone().unwrap();
+                                    options.binding_map.insert(
+                                        binding,
+                                        naga::back::hlsl::BindTarget {
+                                            space: 0,
+                                            register: register_u,
+                                            binding_array_size: None,
+                                        },
+                                    );
+                                    register_u += 1
+                                }
+                                _ => {
+                                    let binding = global_variable.binding.clone().unwrap();
+                                    options.binding_map.insert(
+                                        binding,
+                                        naga::back::hlsl::BindTarget {
+                                            space: 0,
+                                            register: register_t,
+                                            binding_array_size: None,
+                                        },
+                                    );
+                                    register_t += 1
+                                }
+                            }
+                        }
+                        naga::AddressSpace::PushConstant => {
+                            let binding = global_variable.binding.clone().unwrap();
+                            options.binding_map.insert(
+                                binding,
+                                naga::back::hlsl::BindTarget {
+                                    space: 0,
+                                    register: register_b,
+                                    binding_array_size: None,
+                                },
+                            );
+                            register_b += 1
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
             let mut buffer = String::new();
-            let options = naga::back::hlsl::Options::default();
 
             let mut writer = naga::back::hlsl::Writer::new(&mut buffer, &options);
             match writer.write(&module, &info) {
@@ -354,11 +501,12 @@ mod tests {
 
     #[test]
     fn wgsl_simple_to_hlsl() {
-        let source = "@group(0) @binding(0) var output: texture_storage_2d<rgba32float, write>;
+        let source = "@group(0) @binding(0) var output0: texture_storage_2d<rgba32float, write>;
         
+        //@group(0) @binding(0) var output1: array<f32>;
         @compute @workgroup_size(4, 5, 6)
         fn main() {
-            textureStore(output, vec2<u32>(0, 0), vec4<f32>(0.1, 0.2, 0.3, 1));
+            textureStore(output0, vec2<u32>(0, 0), vec4<f32>(0.1, 0.2, 0.3, 1));
         }";
         let source_bytes = source.as_bytes();
         let mut hlsl_len: usize = 0;
@@ -377,5 +525,70 @@ mod tests {
         assert_eq!(error_len, 0);
         assert!(hlsl_len > 0);
         assert!(hlsl_string.contains("[numthreads(4, 5, 6)]"));
+    }
+
+    #[test]
+    fn wgsl_reflection_to_hlsl() {
+        let source = "@group(0) @binding(100)
+        var sampler0: sampler;
+        
+        @group(0) @binding(10)   
+        var source0: texture_2d<f32>;
+        
+        @group(0) @binding(20)
+        var target0: texture_storage_2d<rgba8unorm, write>;
+
+        @group(0) @binding(30)
+        var<uniform> global0: f32;
+
+        @group(0) @binding(40)
+        var<uniform> global1: f32;
+
+        struct Dummy {
+            a: f32,
+            b: f32,
+            c: f32,
+            d: f32
+        };
+
+        @group(0) @binding(50)
+        var<uniform> global3: Dummy;
+
+        @group(0) @binding(60)
+        var<storage> storage0: u32;
+
+        @group(3) @binding(70)
+        var<storage> storage1: Dummy;
+        
+        @compute @workgroup_size(1, 1, 1)
+        fn main(@builtin(global_invocation_id) tid: vec3<u32>)
+        {
+            let color : vec4<f32> = textureSampleLevel(source0, sampler0, vec2<f32>(f32(tid.x), f32(tid.y)), 0.0);
+            textureStore(target0, tid.xy, color * global0 * global1 * global3.d);
+        }";
+        let source_bytes = source.as_bytes();
+        let mut hlsl_len: usize = 0;
+        let mut error_ptr: *const u8 = std::ptr::null_mut();
+        let mut error_len: usize = 0;
+        let hlsl_ptr = compushady_naga_wgsl_to_hlsl(
+            source_bytes.as_ptr(),
+            source_bytes.len(),
+            &mut hlsl_len,
+            &mut error_ptr,
+            &mut error_len,
+        );
+
+        let hlsl_string = compushady_naga_get_utf8(hlsl_ptr, hlsl_len);
+
+        assert_eq!(error_len, 0);
+        assert!(hlsl_len > 0);
+        assert!(hlsl_string.contains("register(b0)"));
+        assert!(hlsl_string.contains("register(b1)"));
+        assert!(hlsl_string.contains("register(b2)"));
+        assert!(hlsl_string.contains("register(t0)"));
+        assert!(hlsl_string.contains("register(t1)"));
+        assert!(hlsl_string.contains("register(t2)"));
+        assert!(hlsl_string.contains("register(u0)"));
+        assert!(hlsl_string.contains("register(s0)"));
     }
 }
